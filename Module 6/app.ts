5df32e1a4c3102e8099ca1db598534e26dbeb561
Taylor Smith <taylor.cody.smith@gmail.com>
2018-12-05 15:44:35 -0500

    wrapped up non-module specific examples

--- Module 6/app.ts
+++ Module 6/app.ts
@@ -36,20 +36,49 @@ console.log(MyMath.calculateCircumference(3));
 
 
 // ************************************* namespaces and multiple files *****************************
-// if you have separate files for namespaces which contain a lot of content... you can just reference every individual file in your html, bundle at compile time, or use imports... 
+// if you have separate files for namespaces which contain a lot of content... you can just reference every individual file in your html
 
 // method one bundle at compile time: 
 // can bundle all of the js compiled ts files into one js file using: 
 // command "tsc --outFile <name of file to create> <name of files to bundle>"
 
 
-// method two: imports 
+// method two: imports // files added by order you type them
 ///<reference path="circleMath.ts" />
 ///<reference path="rectangleMath.ts" />
 
 // can now compile by just using tsc again... still need to use --outfile command option and specify which file you want to group into
 
-// ******************************************* introduction **************************************
+// ******************************************* More on namespaces **************************************
+// can nest namespaces...
+namespace MyMathTwo {
+    export namespace Circle {
+        const PItwo: number = 3.14; 
+    }
+}
+
+// can import namespaces... 
+import math = MyMath;
+
+console.log(math.calculateCircumference(5)); 
+console.log(math.calculateRectangle(4,3)); 
+
+/*
+So that's just some advanced techniques on name spaces but that is in general how you use namespace
+
+is very important to make sure to bundle them into one single file or import all the files here with
+
+script X and also make sure to add the right order of imports here in your main file or manually add
+
+all the files with the TSC command here when compiling it.
+
+But always make sure that you're not forgetting unnecessary imports here.
+*/
+
+
+// *************************************** limitations of namespaces **********************************
+// namespaces are not very declarative of what they need... hard to see which files have what dependencies... 
+// TS namespaces are better for smaller applications as a result of the above
 
 
 
@@ -57,18 +86,9 @@ console.log(MyMath.calculateCircumference(3));
 
 
 
+// *********************************************** Modules ********************************************
 
-
-
-
-
-
-
-
-
-// ******************************************* introduction **************************************
-
-
+// refer to math_modulesExample folder
 
 
 
